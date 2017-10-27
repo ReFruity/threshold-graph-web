96aa902460d27fa8b277d2824823ee4c53a186fc
ReFruity <istandon8@gmail.com>
2017-10-27 15:49:19 +0500

    Serve index.html

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
+const { spawn } = require('child_process');
+const express = require('express');
+const  path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const app = express();
+const thresholdGraph = spawn('bin/threshold_graph.exe');
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/calculate', function(req, res) {
+//  console.log('reg: ', req);
+//  console.log('res ', res);
+  console.log('in calculate')
+  thresholdGraph.stdout.on('data', data => {
+    console.log('data: ', data.toString());
+//    res.send(data.toString())
+  });
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('App listening on port 3000!')
+});
