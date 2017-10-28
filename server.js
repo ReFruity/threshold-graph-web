b1f15f47125310c3842d696a65a609609589e1b7
ReFruity <istandon8@gmail.com>
2017-10-28 18:28:40 +0500

    Calculate responds with result

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const { execFileSync } = require('child_process');
-const http = require('http');
 const html = require('html');
 const fs = require('fs');
 const express = require('express');
@@ -11,7 +10,7 @@ app.use(bodyParser.json());
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let cache = [];
@@ -26,13 +25,18 @@ function circularReferenceResolver (key, value) {
   }
   return value;
 }
+
 app.post('/calculate', function(req, res) {
+  console.log("req.body: ", req.body);
   const resultBuffer = execFileSync('bin/threshold_graph.exe', ["3", "3", "2", "1", "1", "1", "1"]);
+//  console.log(`result:\n${resultBuffer.toString()}`);
 //  fs.writeFileSync('bin/sample', resultBuffer);
-//  fs.writeFileSync('bin/req', JSON.stringify(req, circularReferenceResolver, 2));
+  fs.writeFileSync('bin/req', JSON.stringify(req, circularReferenceResolver, 2));
 //  fs.writeFileSync('bin/res', JSON.stringify(req, circularReferenceResolver, 2));
 //  res.send(resultBuffer.toString())
-  res.render('index', { result: resultBuffer.toString() });
+//  res.render('index', { result: resultBuffer.toString() });
+//  res.render('result', { result: resultBuffer.toString() })
+  res.send(resultBuffer.toString());
 });
 
 app.listen(3000, function () {
